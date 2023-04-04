bc1a01c4087b1656afc52559871603ba8c7ee4f9
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-04-03 20:00:33 -0500

    Create player2 and make it move as well

--- main.js
+++ main.js
@@ -2,12 +2,33 @@ const player1 = document.createElement('div');
 player1.classList.add("player1")
 document.body.append(player1)
 
+const player2 = document.createElement('div');
+player2.classList.add("player2")
+document.body.append(player2)
+
 let player1Position = 40 
+let player2Position = 40 
 
 player1.style.marginTop = `${player1Position}vh`
+player2.style.marginTop = `${player2Position}vh`
 
 document.body.addEventListener("keydown", e => {
-    console.log(e)
-    player1Position += 5
-    player1.style.marginTop = `${player1Position}vh`
+    console.log(e.key)
+    if(e.key == 's'){ //not strict equal to deal with accidental caps lock
+        player1Position += 5
+        player1.style.marginTop = `${player1Position}vh`
+    }
+    if(e.key == 'w'){
+        player1Position -= 5
+        player1.style.marginTop = `${player1Position}vh`
+    }
+    if(e.key == 'ArrowDown'){
+        player2Position += 5
+        player2.style.marginTop = `${player2Position}vh`
+    }
+    if(e.key == 'ArrowUp'){
+        player2Position -= 5
+        player2.style.marginTop = `${player2Position}vh`
+    }
+    
 })

7327d08ad730a858a1d4381d2e86780c601338c4
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2023-04-03 19:51:29 -0500

    Make player move on key down

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 const player1 = document.createElement('div');
 player1.classList.add("player1")
 document.body.append(player1)
+
+let player1Position = 40 
+
+player1.style.marginTop = `${player1Position}vh`
+
+document.body.addEventListener("keydown", e => {
+    console.log(e)
+    player1Position += 5
+    player1.style.marginTop = `${player1Position}vh`
+})
